2b7eb0e1ed70de326f101cf6cd011aac8d9f2f54
winndorrs <winndorrs@gmail.com>
2022-11-09 21:01:40 +0700

    add POST and PUT test case

--- cypress/e2e/network-request.cy.js
+++ cypress/e2e/network-request.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress"/>
 
 describe("Network Requests", () =>{
+    let message = "Unable to find comment!"
     beforeEach(() =>{
         cy.visit("https://example.cypress.io/commands/network-requests");
     })
@@ -16,7 +17,7 @@ describe("Network Requests", () =>{
         cy.wait("@getComment").its("response.statusCode").should("eq", 200); //check status code response from API
     })
 
-    it.only("Get Request Mocking Response", () =>{
+    it("Get Request Mocking Response", () =>{
         cy.intercept({
             method: "GET",
             url: "**/comments/*", },{
@@ -33,4 +34,41 @@ describe("Network Requests", () =>{
 
         cy.wait("@getComment").its("response.statusCode").should("eq", 200); //check status code response from API
     })
-})
+
+    it("Post Request", () =>{
+        cy.intercept({
+            method: "POST",
+            url: "**/comments"
+        }).as("postComment"); //intercept API
+
+        cy.get(".network-post").click(); //find and click button
+
+        cy.wait("@postComment").should(({request, response}) => {
+            //console.log(request);
+            //console.log(response);
+            expect(response.statusCode).to.eq(201);
+            expect(response.body).to.have.property("name", "Using POST in cy.intercept()");
+            expect(request.body).to.include("email");
+            expect(request.headers).to.have.property("content-type");
+        })
+    })
+
+    it.only("Put request", () =>{
+            cy.intercept({
+                method: "PUT",
+                url: "**/comments/*"
+            },
+            {
+                statusCode : 404,
+                body: { error: message},
+                delay: 500
+            }).as("putComment");
+
+        cy.get(".network-put").click();
+
+        cy.wait("@putComment");
+
+        //assertion message
+        cy.get(".network-put-comment").should("contain", message)
+    })
+});
